85aba1db876c32416322ed680106f1aa8f48251b
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-01-15 18:45:08 -0700

    Refactor API to reuse data from requests of the same link

--- app/api/data/nager_data.rb
+++ app/api/data/nager_data.rb
@@ -1,17 +1,18 @@
 class NagerData
 
+  def next_holiday_name
+    @get_next_holidays ||= NagerService.get_next_holidays
+    @get_next_holidays[0]["localName"]
+  end
+
   def next_holidays(amount) # Returns the name and date of the next N holidays
-    @_data ||= service.get_next_holidays
-    @_data.first(amount).map do |holiday|
+    @get_next_holidays ||= NagerService.get_next_holidays
+    @get_next_holidays.first(amount).map do |holiday|
       {
         name: holiday["localName"],
         date: holiday["date"]
       }
     end
-  end
-
-  def service
-    @_service ||= NagerService.new
   end
 
 end
--- app/api/services/nager_service.rb
+++ app/api/services/nager_service.rb
@@ -1,9 +1,9 @@
 class NagerService
-  def get_next_holidays
+  def self.get_next_holidays
     get_url("https://date.nager.at/api/v3/NextPublicHolidays/US")
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = Faraday.get(url)
 
     JSON.parse(response.body)
