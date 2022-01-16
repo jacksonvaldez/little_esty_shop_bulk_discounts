fa4b9d4c07cae8d4563845d2adeef12f9d59d964
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-01-15 21:08:05 -0700

    Refactor API

--- app/api/data/nager_data.rb
+++ app/api/data/nager_data.rb
@@ -1,18 +1,22 @@
 class NagerData
 
   def next_holiday_name
-    @get_next_holidays ||= NagerService.get_next_holidays
-    @get_next_holidays[0]["localName"]
+    data = service.get_next_holidays
+    data[0]["localName"]
   end
 
   def next_holidays(amount) # Returns the name and date of the next N holidays
-    @get_next_holidays ||= NagerService.get_next_holidays
-    @get_next_holidays.first(amount).map do |holiday|
+    data = service.get_next_holidays
+    data.first(amount).map do |holiday|
       {
         name: holiday["localName"],
         date: holiday["date"]
       }
     end
+  end
+
+  def service
+    @service ||= NagerService.new
   end
 
 end
--- app/api/services/nager_service.rb
+++ app/api/services/nager_service.rb
@@ -1,9 +1,9 @@
 class NagerService
-  def self.get_next_holidays
-    get_url("https://date.nager.at/api/v3/NextPublicHolidays/US")
+  def get_next_holidays
+    @get_next_holidays ||= get_url("https://date.nager.at/api/v3/NextPublicHolidays/US")
   end
 
-  def self.get_url(url)
+  def get_url(url)
     response = Faraday.get(url)
 
     JSON.parse(response.body)
